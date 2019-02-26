c5991f09afe24be5ca33fe6a733007bc67f71b6e
volski <volski.alex@gmail.com>
2019-02-26 16:40:56 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,21 @@
+cd /tmp/
+wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+
+echo "$(tput setaf 1)"
+chromedriver --version
+echo "$(tput sgr 0)"
+
+curl https://intoli.com/install-google-chrome.sh | bash
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+echo "$(tput setaf 1)"
+google-chrome --version && which google-chrome
+echo "$(tput sgr 0)"
+
+#install python first
+sudo yum install -y python34-setuptools
+sudo easy_install-3.4 pip
+
+pip3 install selenium --user
+
