74f8a2d13a1dd82d3aa7d9d53113ee612118d1fd
volski <volski.alex@gmail.com>
2019-02-26 17:44:39 +0200

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,10 @@
-ps -ef | grep python > dev/null
-if [ $? -eq 0 ]; then
-  echo "Process is running."
+PROCESS=$1
+PIDS=`ps -ef | grep python $PROCESS | grep -o '^[ ]*[0-9]*'`
+if [ -z "$PIDS" ]; then
+  echo "Process not running." 1>&2
+  exit 1
 else
-  echo "Process is not running."
+  for PID in $PIDS; do
+    echo $PID
+  done
 fi
